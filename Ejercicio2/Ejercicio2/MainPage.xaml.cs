c7ea6d5bbb4d1078296f527b2bf240408f64b5a8
JosueSauceda <106897370+JosueSauceda@users.noreply.github.com>
2023-02-27 22:10:02 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Ejercicio2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            var resultado = new Person
+            {
+                Nombre = txtnombre.Text,
+                Apellidos = txtapellidos.Text,
+                Edad = txtedad.Text,
+                Correo = txtcorreo.Text
+            };
+
+            var enviar = new Page2();
+            enviar.BindingContext = resultado;
+            await Navigation.PushAsync(enviar);
+
+        }
+    }
+}
+
---
+++ Ejercicio2/Ejercicio2/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Model
+{
+    class Person
+    {
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
+
